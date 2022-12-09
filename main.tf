5ca54ab1136e3bb19becf8b4ea1bd45b4c89e963
olga-matusik <olga.matusik@cloudreach.com>
2022-12-09 14:30:45 +0100

    comment on a keypair creation

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ data "aws_nat_gateway" "nat-gw" {
   }
 }
 
+#I created a key-pair here for now, but it would be better to do it manually on the console, but I don't have the access :(
 resource "aws_key_pair" "my-keypair" {
   key_name   = "my-keypair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWKq2T3LmvbkfxdzzCFKGrPNrOTzy6DZ+0LTzuDOVQzFZf4Cs7YhcI14mZI59mqaBp7TTunZHkwlsyiLcyht90m3YynvQM6BfT/7PvHhqMXANwzLuklVYdRUM+OukOisuaF67ayGMgVAKShhvvIJC7IAlUuNOeQXi/5bLaoChayw60wtXe/2BEj1r4k2m1OLrGjmBZpkCSU0pBwwGVBpvooZTvQRSoB8Zzls+SrJySIVTbv+XZf9QZ7rVyE8WCHEQMGq/ktKiRvzwwsGBZvfKyEhPVKe6r8di04ZD+l3k+FFXVebmA+wIcNZZhYdOpO+hXuAr+bcNd9Y33JvColnzNgVcos5+021aFp0aPKZ0YHnhq7d+1VgNNjFYE/a1tPAKbxjzAZSUvzJ53oJ20vn59vykssj1vkGAF1JazGJYSi5MKYWxPRzD+PgVWrEtwJXSJD6s2XVSEQLi76YMn0tZZYV91Ho3YkxL0LzuO8rAdxHRIscuSlhX3IozdOMXa5Es= olga.matusik@Olga-Matusik's-MacBook---CYH4QPK7FDß"
